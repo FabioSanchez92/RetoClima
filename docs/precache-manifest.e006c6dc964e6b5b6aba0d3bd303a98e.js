b5d4edd301911d5a79281364e3716cd7eb40555f
Fabio Sánchez <Fabio_developer@hotmail.com>
2020-10-14 20:18:32 -0500

    se cambia el nombre de la carpeta build para poder desplegar en GitHub

---
+++ docs/precache-manifest.e006c6dc964e6b5b6aba0d3bd303a98e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433dbbff16633050cf24d775838a0179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc4c36c07588093c46b",
+    "url": "/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "57043a6ed0c76afd1844",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "adc4c36c07588093c46b",
+    "url": "/static/js/2.45b08d15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45b08d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57043a6ed0c76afd1844",
+    "url": "/static/js/main.51bfd1ea.chunk.js"
+  },
+  {
+    "revision": "f4939dec7f2baf29f235",
+    "url": "/static/js/runtime-main.6dfd344b.js"
+  }
+]);
